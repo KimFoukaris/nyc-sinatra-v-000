0f7ec56bf34a692be59b04a35dee28df005d7956
Kim Foukaris <kimfoukaris@outlook.com>
2018-04-26 12:15:14 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -18,6 +18,7 @@ class FiguresController < ApplicationController
     @figure=Figure.create(params[:figure])
     if params[:landmark]["name"] != ""
       @landmark=Landmark.create(params[:landmark])
+      binding.pry
       @figure.landmarks << @landmark.id
       binding.pry
     end
